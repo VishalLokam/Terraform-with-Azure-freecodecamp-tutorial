a9f40f18723af2d92e968b47d065adfc257c6bfb
VishalLokam <vishalvikaslokam@gmail.com>
2023-12-30 22:24:03 +0530

    Created virtual network

--- main.tf
+++ main.tf
@@ -13,9 +13,20 @@ provider "azurerm" {
 
 
 #This will create a new resource group in Azure
-resource "azurerm_resource_group" "tf_azure_rg_1" {
-  name     = "tf_azure_rg_1"
+resource "azurerm_resource_group" "tf_az_rg_1" {
+  name     = "tf_az_rg_1"
   location = "centralindia"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "tf_az_vn_1" {
+  name                = "tf_az_vn_1"
+  resource_group_name = azurerm_resource_group.tf_az_rg_1.name
+  location            = azurerm_resource_group.tf_az_rg_1.location
+  address_space       = ["10.123.0.0/16"]
+
   tags = {
     environment = "dev"
   }
